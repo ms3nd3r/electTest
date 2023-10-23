d15d60c59a93ae3c037926c53914f54b7f6ac774
ms3nd3r <casm1200160@gn.iwasaki.ac.jp>
2023-10-23 14:29:45 +0900

    stop-watch-by-electronの仕様をコピー

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,94 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
-import './App.css';
+// import './App.css';
 
-function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
+// function Hello() {
+//   return (
+//     <div>
+//       <div className="Hello">
+//         <img width="200" alt="icon" src={icon} />
+//       </div>
+//       <h1>test</h1>
+
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   return (
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Hello />} />
+    //   </Routes>
+    // </Router>
+//     <div>あ</div>
+//   );
+// }
+
+// import React from 'react';
+import logo from '../../assets/icon.svg';
+import './App.css';
+import {useStopwatch} from "react-timer-hook";
+
+const ResetButton = (props: {clickEvent:() => void}): JSX.Element => {
+    return <button onClick={props.clickEvent}>RESET</button>
+}
+
+const StartPauseButton = (props: {running: boolean, clickEvent: () => void}): JSX.Element => {
+    if (!props.running) {
+        return (<button onClick={props.clickEvent}>START</button>)
+    }
+
+    return (<button onClick={props.clickEvent}>PAUSE</button>)
+}
+
+const elapse = (running: boolean, hours: number, minutes: number, seconds:number): string => {
+
+return hours.toString().padStart(2, '0')
+    + ':' + minutes.toString().padStart(2, '0')
+    + ':' + seconds.toString().padStart(2, '0');
 }
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
+    const {
+        seconds,
+        minutes,
+        hours,
+        isRunning,
+        start,
+        pause,
+        reset
+    } = useStopwatch({autoStart: false});
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/> Stop Watch
+            </header>
+            <section className='main'>
+                <div>
+                    <div className='timer'>
+                        {elapse(isRunning, hours, minutes, seconds)}
+                    </div>
+                </div>
+                <div className='buttons'>
+                    <ResetButton clickEvent={() => {
+                        // 2回ボタンを押すとちゃんとリセットされるんだけど…
+                        reset();
+                        pause();
+                    }} />
+                    <StartPauseButton running={isRunning} clickEvent={() => {
+                        if (isRunning) {
+                            pause();
+                        } else {
+                            start();
+                        }
+                    }} />
+                </div>
+            </section>
+            <footer>
+                <a className='App-link' href='https://github.com/satoshi-nishinaka/stop-watch'>satoshi-nishinaka/stop-watch</a>
+            </footer>
+        </div>
+    );
 }
